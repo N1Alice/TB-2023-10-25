6640c0dbdfab410e342cac66f75df3b9c4d31ce7
N1Alice <kc700370@gmail.com>
2023-10-25 18:08:10 +0900

    스캐너 도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,18 @@
 package com.ll;
 
-class App {
+import java.util.Scanner;
+
+public class App {
     void run() {
-        System.out.println("Program running");
+        System.out.println("== 명언 앱 ==");
+
+        System.out.print("명령) ");
+
+        Scanner scanner = new Scanner(System.in);
+        String cmd = scanner.nextLine();
+
+        System.out.printf("입력하신 명령 : %s\n", cmd);
+        // 위 라인은 아래 라인과 같은 뜻
+        // System.out.println("입력하신 명령 : " + cmd + "\n");
     }
 }
