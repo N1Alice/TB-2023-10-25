ed267017fe8f87ee7c30e0dc5cc6ab4b72897513
N1Alice <kc700370@gmail.com>
2023-10-26 11:31:08 +0900

    step 5

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,10 +1,13 @@
 package com.ll;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     void run() {
         int loop_count = 0;
+        ArrayList<String> wsc = new ArrayList<>();
+        ArrayList<String> a = new ArrayList<>();
         System.out.println("== 명언 앱 ==");
 
         while (true) {
@@ -13,18 +16,37 @@ public class App {
             String cmd = scanner.nextLine();
 
             if (cmd.equals("종료")) {
+
                 break;
             } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                Scanner wsc = new Scanner(System.in);
+                Scanner wscinput = new Scanner(System.in);
                 String Wisesaying = scanner.nextLine();
+                wsc.add(Wisesaying);
+
                 System.out.print("작가 : ");
-                Scanner a = new Scanner(System.in);
+                Scanner ainput = new Scanner(System.in);
                 String Athor = scanner.nextLine();
+                a.add(Athor);
+
+
                 loop_count = loop_count + 1;
+
+                //System.out.printf("명언 : %s\n" + "작가 : %s\n", Wisesaying, Athor);
 
                 System.out.println(loop_count + "번 명언이 등록되었습니다.");
 
+                continue;
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("------------------------------------------");
+
+                for(int i = 0; i < wsc.size(); i++){
+                    String wscs = wsc.get(i);
+                    String as = a.get(i);
+
+                    System.out.printf("%d /" + "%s /" + "%s \n", i+1, as, wscs);
+                }
                 continue;
             }
         }
