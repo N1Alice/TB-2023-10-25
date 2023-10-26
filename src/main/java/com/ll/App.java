a390451f4737fa37b3b9f53557c142af6752aec1
N1Alice <kc700370@gmail.com>
2023-10-26 15:31:32 +0900

    step8

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -6,27 +6,24 @@ import java.util.Scanner;
 public class App {
     void run() {
         int loop_count = 0; // 루프 카운트 변수
-        int delete_count = 0; // 삭제 넘버 등록 변수
         ArrayList<String> wsc = new ArrayList<>(); // 명언 저장 배열
         ArrayList<String> a = new ArrayList<>();  // 작가 저장 배열
+        Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
 
         while (true) {
             System.out.print("명령) ");
-            Scanner scanner = new Scanner(System.in);
-            String cmd = scanner.nextLine();
+            String cmd = scanner.nextLine(); // cmd = 입력받은 명령
 
             if (cmd.equals("종료")) {
 
                 break;
-            } else if (cmd.equals("등록")) {
+            } if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                Scanner wscinput = new Scanner(System.in);
                 String Wisesaying = scanner.nextLine();
                 wsc.add(Wisesaying);
 
                 System.out.print("작가 : ");
-                Scanner ainput = new Scanner(System.in);
                 String Athor = scanner.nextLine();
                 a.add(Athor);
 
@@ -38,29 +35,34 @@ public class App {
                 System.out.println(loop_count + "번 명언이 등록되었습니다.");
 
                 continue;
-            } else if (cmd.equals("목록")) {
+            } if (cmd.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("------------------------------------------");
                 // 목록 출력 반복문
-                for(int i = 0; i < wsc.size(); i++){
+                for(int i = 0; i < wsc.size(); ++i){
                     String wscs = wsc.get(i);
                     String as = a.get(i);
 
+                    if (wsc.get(i) == null){ // 비어있는 배열의 경우 넘긴다.
+                        continue;
+                    }
                     System.out.printf(" %d / " + " %s / " + " %s \n", i+1, as, wscs);
                 }
                 continue;
-            } else if (cmd.equals("삭제")) {
+            } if (cmd.equals("삭제")) {
 
                 System.out.print("번호 : ");
-                Scanner deletenum = new Scanner(System.in);
-                int Dn = scanner.nextInt();
+                //int Dn = scanner.nextInt(); // 스캐너의 고질적인 문제
+                int Dn  = Integer.parseInt(scanner.nextLine());
 
-                if (wsc.get(Dn-1) == null) // 배열 값 확인
+                if (wsc.get(Dn-1) == null) // 배열 값 비교확인
                 {
-                    System.out.println(Dn + "번 명은은 존재하지 않습니다");
+                    System.out.println(Dn + "번 명언은 존재하지 않습니다"); //
                     continue;
 
-                } else if (wsc.get(Dn) != null) {
+                }
+                else if (wsc.get(Dn-1) != null) // 배열 값 비교확인
+                {
                     wsc.set(Dn-1, null);
                     a.set(Dn-1, null);
 
@@ -68,6 +70,27 @@ public class App {
                     continue;
                 }
 
+            } if (cmd.equals("수정")) {
+                System.out.print("번호 : ");
+                int Cn = Integer.parseInt(scanner.nextLine()); // Scanner의고질적인 문제
+                //nextInt로 받는경우 Enter입력값이 계속 남아있기에 이후에 입력받을때 자동으로 엔터가 입력된다.
+                System.out.println("명언(기존) : " + wsc.get(Cn-1));
+                System.out.print("명언 : ");
+                String Cws = scanner.nextLine();
+                wsc.set(Cn-1, Cws);
+
+                System.out.println("작가(기존) : " + a.get(Cn-1));
+                System.out.print("작가 : ");
+                String Ca = scanner.nextLine();
+                a.set(Cn-1, Ca);
+
+
+                continue;
+            }
+
+            else{
+                System.out.println("등록되지 않은 명령 입니다.");
+                continue;
             }
         }
     }
